42f84e5de4e0bf2ff6c5f5f80b7a6b0b228c8615
root <root@1bae8a591785>
2018-10-24 17:40:19 +0000

    adding to mongodb via os.system

--- database_creation_tools/create_database_nuclear.py
+++ database_creation_tools/create_database_nuclear.py
@@ -14,8 +14,8 @@ import numpy as np
 from nuc_data_lib import *
 
 def make_json_obj_from_nuclear_data_file(root,file):
-    if 'exfor' in root:   
-        data = make_json_obj_from_exfor_file(root,file) 
+    if 'exfor' in root:
+        data = make_json_obj_from_exfor_file(root,file)
     else:
         data = make_json_obj_from_evaluation_file(root,file)
     return data
@@ -23,7 +23,7 @@ def make_json_obj_from_nuclear_data_file(root,file):
 def make_json_obj_from_evaluation_file(root,file):
     print('getting data from ',root, file)
     chop_up = root.split(os.sep)
-    mt_number=file 
+    mt_number=file
     library = chop_up[-1]
     nucleon_number = chop_up[-2]
     element = chop_up[-3]
@@ -46,7 +46,7 @@ def make_json_obj_from_evaluation_file(root,file):
 
     if products.startswith('none found') ==False:
 
-        #textfield =  element_full+ ' '+element+' ' +nucleon_number +' '+incident_particle+','+products+  ' MT'+mt_number +' '+ library 
+        #textfield =  element_full+ ' '+element+' ' +nucleon_number +' '+incident_particle+','+products+  ' MT'+mt_number +' '+ library
         energy,xs, =np.genfromtxt(fname=os.path.join(root, file), usecols = (0, 1) ,unpack=True,comments='#')
         data = {'Element':element_full,
                 'Element symbol':element.title(),
@@ -57,7 +57,7 @@ def make_json_obj_from_evaluation_file(root,file):
                 'Products':products,
                 'MT number':mt_number,
                 'Library':library,
-                'filename':element.title()+nucleon_number+' ('+incident_particle+','+products+') '+ library,
+                'filename':str(element.title()+nucleon_number+' '+incident_particle+','+products+' '+ library).replace(' ','_'),
                 'energy':list(energy),
                 'xs':list(xs),
                 #'textfield':textfield
@@ -71,7 +71,7 @@ def make_json_obj_from_exfor_file(root,file):
     experiment_id  =chop_up[-1].split('-')[1]
     experiment_year=chop_up[-1].split('-')[2]
     mt_number=chop_up[-2]
-    library = chop_up[-3]           
+    library = chop_up[-3]
     nucleon_number = chop_up[-4]
 
     if nucleon_number.endswith('m') or nucleon_number.endswith('n'):
@@ -85,7 +85,7 @@ def make_json_obj_from_exfor_file(root,file):
     products = mtlookup(incident_particle,mt_number)
 
     if products.startswith('none found')==False:
-      
+
       if nucleon_number == '000':
         neutron_number='natural'
         nucleon_number='natural'
@@ -95,13 +95,13 @@ def make_json_obj_from_exfor_file(root,file):
           print('neutron number can not be negative ',neutron_number)
           print('chop_up',chop_up)
           print('protons_and_neutrons',protons_and_neutrons)
-          print('proton_number',proton_number)                
+          print('proton_number',proton_number)
           print(root,dirs,file)
           sys.exit()
 
       element_full = element_lookup(element).lower()
       energy,xs,error_xs,error_energy =np.genfromtxt(fname=os.path.join(root, file), usecols = (0, 1, 2, 3) ,unpack=True,comments='#')
- 
+
       data = {'element_full':element_full,
               'element':element,
               'nucleon_number':nucleon_number,
@@ -117,9 +117,9 @@ def make_json_obj_from_exfor_file(root,file):
               'xs':np.array2string(xs).replace('\n', ''),
               'error_xs':np.array2string(error_xs).replace('\n', ''),
               'error_energy':np.array2string(error_energy).replace('\n', ''),
-              'energy':np.array2string(energy).replace('\n', ''),                   
+              'energy':np.array2string(energy).replace('\n', ''),
               #'textfield':textfield
-             }    
+             }
       return data
 
 def make_json_objs_from_files(list_of_files):
@@ -129,7 +129,7 @@ def make_json_objs_from_files(list_of_files):
     for dir_and_file in list_of_files:
         root,file =os.path.split(dir_and_file)
         list_of_data.append(make_json_obj_from_nuclear_data_file(root,file))
-                  
+
     return list_of_data
 
 def save_list_of_all_files(list_of_data,filename):
@@ -147,15 +147,19 @@ print(list_of_csv_filenames)
 
 list_of_json_objects = make_json_objs_from_files(list_of_csv_filenames)
 
+for i in list_of_json_objects:
+    save_list_of_all_files(i,i['filename']+'.json')
+    os.system('mongoimport --collection small_punch --db materials_database --file '+i['filename']+'.json')
 print(list_of_json_objects[0])
 
 
-collection, client, db = connect_to_docker_database()
+
+#collection, client, db = connect_to_docker_database()
 
 #delete_database(client)
 
-upload_json_objects_to_database(list_of_json_objects, collection)
+#upload_json_objects_to_database(list_of_json_objects, collection)
 
-all_database_fields = get_database_fields(collection)
+#all_database_fields = get_database_fields(collection)
 
-print('all_database_fields',all_database_fields)
+#print('all_database_fields',all_database_fields)
